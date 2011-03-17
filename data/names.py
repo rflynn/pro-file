ee47f29b395f9bbfeed2c10c9f3f248d357d0c0d
pizza <parseerror@gmail.com>
2011-03-17 18:17:28 -0400

    split burgeoning surname-origin.py into hint.py, names.py and givenname-origin.py. data is consistent, but i need logic to categorize hints

---
+++ data/names.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Operations common to names
+"""
+
+import re
+
+# normalize spaces, case and apostrophes, but leave accented characters alone
+def strip(name):
+	name = re.sub('\s+', '', name) # remove spaces
+	name = re.sub("['’]", '', name) # nuke apostrophes
+	name = name.upper()
+	return name
+
+# completely normalize name
+def normalize(name):
+	name = strip(name)
+	# de-accent whacky foreign characters
+	for (r,p) in [
+		('A', '[ÀÁÂÄÃẦĀ]'),
+		('C', '[Ç]'),
+		('E', '[ÈÉÊËỄỆ]'),
+		('I', '[ÌÍÎÏĪ]'),
+		('O', '[ÒÓÔÕÖỖƠŌ]'),
+		('N', '[Ñ]'),
+		('U', '[ÙÚÛÜŨƯ]'),
+		('Y', '[ÝŸỲ]') ]:
+		name = re.sub(p, r, name, re.UNICODE)
+	return name
