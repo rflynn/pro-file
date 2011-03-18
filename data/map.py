316d6edbee36bb28d2bfcd2b84ccb8de198f7507
pizza <parseerror@gmail.com>
2011-03-18 03:01:33 -0400

    added small world map and code to highlight countries by cc

---
+++ data/map.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import xml.etree.ElementTree as ET
+
+def fill(infile, outfile, paths):
+	tree = ET.ElementTree()
+	root = tree.parse(open(infile))
+	#print(root)
+	# FIXME: i want find("/path[@id='foo']") but this piece of shit doesn't support it
+	# FIXME: not sure why it requires namespace... but it does.
+	for p in root.findall('{http://www.w3.org/2000/svg}path'):
+		id = p.get('id')
+		if id in paths:
+			p.set('style', 'fill:' + paths[id])
+			#print('set %s ' % (id))
+	#text = ET.tostring(root)
+	#print(text)
+	tree.write(outfile)
+
+def highlight_countries(ccs, rgb='#000', infile='map/world-small.svg', outfile='map/world-small-out.svg'):
+	fill(infile, outfile, dict(zip(ccs, [rgb] * len(ccs))))
+
+if __name__ == '__main__':
+	highlight_countries('us ca mx br cn ru jp uk ie nl se no fr de es pt pl ro gr cz il eg in pk lk au mm th kh la'.split(' '))
+
