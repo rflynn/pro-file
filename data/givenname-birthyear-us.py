70284b41fa487ac357b71ba3d07102358ee86cd6
pizza <parseerror@gmail.com>
2011-03-19 15:56:26 -0400

    direct sqlite access instead of printing

--- data/givenname-birthyear-us.py
+++ data/givenname-birthyear-us.py
@@ -13,6 +13,8 @@ i = url.rfind('/')
 file = url[i+1:]
 dir = 'givenname-birthyear-us'
 
+# TODO: consolidate following blocks, shared in other places, into utility functions
+
 if os.path.exists(file):
 	print('-- %s exists' % (file))
 else:
@@ -33,7 +35,24 @@ else:
 	subprocess.call(['unzip', '-d', dir, file])
 	print('*/')
 
-print("""
+import glob, re
+import names
+
+def yob_generator():
+	files = glob.glob('./' + dir + '/yob*.txt')
+	for filename in files:
+		year = re.search('yob(\d+)\.txt', filename).group(1)
+		if int(year) >= 1900:
+			for line in open(filename, 'r').readlines():
+				(name,gender,total) = line.strip().split(",")
+				norm = names.normalize(name)
+				yield (year, norm, gender, total)
+
+import sqlite3
+
+with sqlite3.connect('./names.db') as conn:
+	c = conn.cursor()
+	c.executescript("""
 DROP TABLE IF EXISTS givenname_birthyear;
 CREATE TABLE givenname_birthyear (
 	year int not null,
@@ -42,30 +61,17 @@ CREATE TABLE givenname_birthyear (
 	total int not null
 );
 
+-- ugly pragmas to speed large insertion
 PRAGMA synchronous = OFF;
 PRAGMA temp_store = MEMORY;
 PRAGMA journal_mode = OFF;
-PRAGMA count_changes = OFF;
-
-BEGIN DEFERRED TRANSACTION;
 """)
-
-import glob, re
-import names
-files = glob.glob('./' + dir + '/yob*.txt')
-for filename in files:
-	year = re.search('yob(\d+)\.txt', filename).group(1)
-	print('-- ' + year)
-	if int(year) >= 1900:
-		for line in open(filename, 'r').readlines():
-			(name,gender,total) = line.strip().split(",")
-			norm = names.normalize(name)
-			print("INSERT INTO givenname_birthyear VALUES" +
-				"(%s,'%s','%s',%s);" % (year, norm, gender, total))
-print("""
-COMMIT;
+	print('-- inserting yob records...')
+	c.executemany('INSERT INTO givenname_birthyear VALUES(?,?,?,?)', yob_generator())
+	print('-- creating index...')
+	c.executescript("""
 CREATE INDEX idx_name ON givenname_birthyear (name);
 PRAGMA synchronous = DEFAULT;
-PRAGMA temp_store = NORMAL;
-""")
+PRAGMA temp_store = NORMAL;""")
+	c.close()
 
