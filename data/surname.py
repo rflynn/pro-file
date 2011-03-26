b5e087f264abc5bb91def338729140228142cdea
pizza <parseerror@gmail.com>
2011-03-26 15:56:05 -0400

    handle hypenated surnames as separate names

--- data/surname.py
+++ data/surname.py
@@ -35,13 +35,19 @@ import names
 
 def lookup(conn, surnames):
 	for name in surnames:
-		norm = names.normalize(name)
-		e = ethnicity(conn, norm)
-		o = origin(conn, norm)
-		print('%-15s %5.2f%% %-6s %s' % (name,e[1],e[0],' '.join(o)))
+		# handle hyphenated surnames
+		for subname in name.split('-'):
+			norm = names.normalize(subname)
+			e = ethnicity(conn, norm)
+			o = origin(conn, norm)
+			print('%-15s %5.2f%% %-6s %s' % (subname,e[1],e[0],' '.join(o)))
 
 def test(conn):
-	lookup(conn, 'Non-existent Smith Nguyen Washington'.split(' '))
+	lookup(conn, 'Non-existent Smith Nguyen Washington Johnson-Johnsson Smithford'.split(' '))
+	# hyphenated married names
+	lookup(conn, 'Johnson-Johnsson'.split(' '))
+	# name blending
+	lookup(conn, 'Smithford'.split(' '))
 
 if __name__ == '__main__':
 	import sys
