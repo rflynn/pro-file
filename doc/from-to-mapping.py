946a91ae94f84b50ab45741a2f1bc1c02f72b982
pizza <parseerror@gmail.com>
2011-04-12 14:44:00 -0400

    clean up potential mappings

--- doc/from-to-mapping.py
+++ doc/from-to-mapping.py
@@ -4,33 +4,33 @@
 #
 
 Fields = (
-	('IP Address', {'City':'probably', 'State':'yes', 'Zip':'yes', 'Country':'yes'}),
-	('Given Name', {'Gender':'99%', 'Age':'50-90%', 'Ethnicity':'maybe', 'Religion':'maybe'}),
-	('Family Name', {'Ethnicity':'maybe','Religion':'maybe'}),
-	('Title', {'Gender':'yes', 'Relationship':'maybe'}),
-	('Email', {'Given Name':'sometimes', 'Family Name':'sometimes', 'City':'sometimes', 'Hobby':'sometimes', 'D.O.B.':'maybe'}),
-	('Age', {'Politics':'probably', 'Home Ownership':'maybe', 'Salary':'maybe'}),
-	('D.O.B.', {'Age':'100%'}),
-	('Address', {'Home Ownership':'sometimes'}),
-	('City', {'State':'maybe'}),
-	('State', {}),
-	('Zip', {'City':'100%', 'State':'100%'}),
-	('Country', {'Language':'probably','Ethnicity':'maybe','Religion':'maybe'}),
-	('Gender', {}),
-	('Language', {'Ethnicity':'maybe'}),
-	('Ethnicity', {'Religion':'maybe', 'Employment':'maybe', 'Education':'maybe', 'Politics':'maybe', 'Language':'probably'}),
-	('Religion', {'Ethnicity':'maybe','Holiday':'100%'}),
-	('Holiday', {'Religion':'maybe','Country':'maybe'}),
-	('Salary', {}),
-	('Employment', {}),
-	('Education', {}),
-	('Hobby', {}),
-	('Relationship', {}),
-	('Home Ownership', {}),
-	('Children', {}),
-	('Politics', {}),
-	('Writing', {'Gender':'probably', 'Education':'maybe', 'Ethnicity':'maybe', 'Relationship':'maybe', 'Hobby':'maybe'}),
-	('Shopping History', {'Gender':'probably', 'Ethnicity':'maybe', 'Religion':'maybe', 'Salary':'maybe', 'Hobby':'probably', 'Relationship':'maybe','Home Ownership':'maybe'}),
+	('IP Address',{'City':'65%','State':'95%','Zip':'60%','Country':'100%'}),
+	('Given Name',{'Gender':'99%','Age':'50-90%','Ethnicity':'60-95%','Religion':'maybe'}),
+	('Family Name',{'Ethnicity':'60-95%','Religion':'maybe'}),
+	('Title',{'Gender':'yes','Relationship':'maybe'}),
+	('Email',{'Given Name':'sometimes','Family Name':'sometimes','City':'sometimes','Hobby':'sometimes','D.O.B.':'maybe'}),
+	('Age',{'Politics':'probably','Home Ownership':'maybe','Salary':'maybe'}),
+	('D.O.B.',{'Age':'100%'}),
+	('Address',{'Home Ownership':'sometimes'}),
+	('City',{'State':'maybe'}),
+	('State',{}),
+	('Zip',{'City':'100%','State':'100%'}),
+	('Country',{'Language':'probably','Ethnicity':'maybe','Holiday':'probably','Religion':'maybe'}),
+	('Gender',{}),
+	('Language',{'Ethnicity':'maybe'}),
+	('Ethnicity',{'Religion':'maybe','Employment':'maybe','Education':'maybe','Politics':'maybe','Language':'probably'}),
+	('Religion',{'Ethnicity':'maybe','Holiday':'maybe'}),
+	('Holiday',{'Religion':'maybe','Country':'maybe'}),
+	('Salary',{}),
+	('Employment',{}),
+	('Education',{}),
+	('Hobby',{}),
+	('Relationship',{}),
+	('Home Ownership',{}),
+	('Children',{}),
+	('Politics',{}),
+	('Writing',{'Gender':'probably','Education':'maybe','Ethnicity':'maybe','Relationship':'maybe','Hobby':'maybe'}),
+	('Shopping History',{'Gender':'probably','Ethnicity':'maybe','Religion':'maybe','Salary':'maybe','Hobby':'probably','Relationship':'maybe','Home Ownership':'maybe','Holiday':'yes'}),
 )
 
 def html():
@@ -56,11 +56,12 @@ def graphviz():
 	print('digraph {')
 	for src,d in Fields:
 		for dst,prob in d.items():
-			print('"%s" -> "%s" [label="%s",style="%s",color="%s"]' % \
+			print('"%s" -> "%s" [label="%s",style="%s",color="%s",penwidth=%.1f]' % \
 				(src,dst,
-				 prob if prob not in ['probably','sometimes','maybe','yes','100%'] else '',
+				 prob if prob not in ['probably','sometimes','maybe','yes'] else '',
 				 'dashed' if prob in ['maybe','sometimes'] else 'solid',
-				 '#cccccc' if prob in ['maybe','sometimes'] else '#000000'))
+				 '#cccccc' if prob in ['maybe','sometimes'] else '#000000',
+				 1. if prob in ['maybe','sometimes'] else 2.))
 	print('}')
 
 if __name__ == '__main__':
